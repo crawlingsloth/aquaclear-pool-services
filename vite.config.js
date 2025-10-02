57c5a9f9b8f360f5e387c60e83588d3d9ea95783
Eshan Shafeeq <eshanshafeeq073055@gmail.com>
2025-10-02 15:11:53 +0500

    final changes done

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['three', '@react-three/fiber', '@react-three/drei']
-  }
-})
+    include: ["three", "@react-three/fiber", "@react-three/drei"],
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+});
+
